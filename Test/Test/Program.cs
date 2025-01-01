d32a35da5e9a608fcbcd2cc6207f0680cd248b5a
Sumin Cho <sumin.cho@unity3d.com>
2025-01-01 22:51:26 +0900

    Second

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 10;
+        Console.WriteLine("Hello, World!");
+    }
+}
